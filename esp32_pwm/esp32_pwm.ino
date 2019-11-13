b8ed238722292fe3a327d481b360d53388a46428
Omkar Sheral <osheral@thoughtworks.com>
2019-11-13 12:43:24 +0530

    [esp32_pwm] [slowly brightens the LED]

--- esp32_pwm/esp32_pwm.ino
+++ esp32_pwm/esp32_pwm.ino
@@ -1,13 +1,15 @@
 #define PWM_LED_PIN 19
 
+const int analogChannel0=0;
+
 void setup()
 {
     //setup channel 0 with frequency 312500 Hz
-    sigmaDeltaSetup(0, 312500);
+    sigmaDeltaSetup(analogChannel0, 312500);
     //attach pin 18 to channel 0
-    sigmaDeltaAttachPin(PWM_LED_PIN,0);
+    sigmaDeltaAttachPin(PWM_LED_PIN, analogChannel0);
     //initialize channel 0 to off
-    sigmaDeltaWrite(0, 0);
+    sigmaDeltaWrite(analogChannel0, 0);
 }
 
 void loop()
@@ -15,6 +17,6 @@ void loop()
     //slowly ramp-up the value
     //will overflow at 256
     static uint8_t i = 0;
-    sigmaDeltaWrite(0, i++);
+    sigmaDeltaWrite(analogChannel0, i++);
     delay(10);
 }
