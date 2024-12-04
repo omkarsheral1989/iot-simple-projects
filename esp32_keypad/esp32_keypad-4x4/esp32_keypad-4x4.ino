5c44d77134fe1c747e253291c557b76ec247cc43
Omkar Sheral <osheral@thoughtworks.com>
2024-12-04 18:39:26 +0530

    ESP32 4x4 Keypad

---
+++ esp32_keypad/esp32_keypad-4x4/esp32_keypad-4x4.ino
@@ -0,0 +1,42 @@
+
+#include <Keypad.h>
+
+//   1       2       3       A
+// 
+//   4       5       6       B
+// 
+//   7       8       9       C
+// 
+//   *       0       #       D
+// 
+// ☉   ☉   ☉   ☉   ☉   ☉   ☉   ☉
+// 13  12  14  27  26  25  33  32
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //three columns
+char keys[ROWS][COLS] = {
+  {'1','4','7','*'},
+  {'2','5','8','0'},
+  {'3','6','9','#'},
+  {'A','B','C','D'}
+};
+byte rowPins[4] = {13, 12, 14, 27};
+byte colPins[4] = {26, 25, 33, 32};
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+void setup(){
+  Serial.begin(9600);
+  while (!Serial) {
+    delay(20); // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Serial.println("\n\nInitializing...");
+}
+  
+void loop(){
+  char key = keypad.getKey();
+  
+  if (key){
+    Serial.println(key);
+  }
+}
