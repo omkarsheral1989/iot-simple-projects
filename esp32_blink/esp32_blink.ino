d4a1dfa812347edfbf8c577c72e270d9c1316d3c
Omkar Sheral <osheral@thoughtworks.com>
2024-12-20 23:15:42 +0530

    esp32 blink: added serial logs

--- esp32_blink/esp32_blink.ino
+++ esp32_blink/esp32_blink.ino
@@ -5,12 +5,19 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_PIN, OUTPUT);
+
+  Serial.begin(9600);
+   while (!Serial) {
+    delay(20); // wait for serial port to connect. Needed for native USB port only
+  }
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_PIN, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_PIN, HIGH);
+  Serial.println("ON");
   delay(500);
-  digitalWrite(LED_PIN, HIGH);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_PIN, LOW);
+  Serial.println("OFF");
   delay(500);
 }
